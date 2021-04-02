cc624bce745e22d3f03fb1fdd1850df2f65332e3
KKKONTR <81813460+KKKONTR@users.noreply.github.com>
2021-04-02 15:56:21 +0300

    Add files via upload

---
+++ Cat.java
@@ -0,0 +1,58 @@
+
+public class Cat
+{
+    private double originWeight;
+    private double weight;
+
+    private double minWeight;
+    private double maxWeight;
+
+    public Cat()
+    {
+        weight = 1500.0 + 3000.0 * Math.random();
+        originWeight = weight;
+        minWeight = 1000.0;
+        maxWeight = 9000.0;
+
+    }
+
+    public void meow()
+    {
+        weight = weight - 100;
+        System.out.println("Мяукнул(а)");
+    }
+
+    public void feed()
+
+    {
+        weight = weight + 100;
+        System.out.println("покормили");
+    }
+
+    public void drink(Double amount)
+
+    {
+        weight = weight + 3;
+    }
+
+    public Double getWeight()
+    {
+        return weight;
+    }
+
+    public String getStatus()
+    {
+        if(weight < minWeight) {
+            return "Dead";
+        }
+        else if(weight > maxWeight) {
+            return "Exploded";
+        }
+        else if(weight > originWeight) {
+            return "Sleeping";
+        }
+        else {
+            return "Playing";
+        }
+    }
+}
---
+++ Loader.java
@@ -0,0 +1,65 @@
+
+public class Loader
+{
+    public static void main(String[] args)
+    {
+        Cat vitya = new Cat();
+
+        System.out.println("Вес Вити" + vitya.getWeight());
+
+        Cat sasha = new Cat();
+
+        System.out.println("Вес Саши" + sasha.getWeight());
+
+        Cat masha = new Cat();
+
+        System.out.println("Вес Маши" + masha.getWeight());
+
+        Cat dasha = new Cat();
+
+        System.out.println("Вес Даши" + dasha.getWeight());
+
+        Cat olya = new Cat();
+
+        System.out.println("Вес Оли" + olya.getWeight());
+
+        Cat saske = new Cat();
+
+        System.out.println("Вес Саске" + saske.getWeight());
+
+        saske.feed();saske.feed();
+
+        System.out.println("Вес Саске" + saske.getWeight());
+        System.out.println(saske.getStatus());
+
+        Cat naruyama = new Cat();
+
+        System.out.println("Вес Наруямы" + naruyama.getWeight());
+
+        naruyama.feed();naruyama.feed();
+
+        System.out.println("Вес Наруямы" + naruyama.getWeight());
+        System.out.println(naruyama.getStatus());
+
+
+
+        Cat murka = new Cat();
+
+        System.out.println("Вес Мурки" + murka.getWeight());
+
+        murka.meow();murka.meow();murka.meow();murka.meow();murka.meow();murka.meow();murka.meow();murka.meow();murka.meow();murka.meow();murka.meow();murka.meow();murka.meow();murka.meow();murka.meow();murka.meow();murka.meow();murka.meow();murka.meow();murka.meow();murka.meow();murka.meow();murka.meow();murka.meow();murka.meow();
+
+        System.out.println("Вес Мурки" + murka.getWeight());
+
+        System.out.println(murka.getStatus());
+
+        Cat kesha = new Cat();
+
+        System.out.println("Вес Кеши" + kesha.getWeight());
+
+        kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();kesha.feed();
+
+        System.out.println("Вес Кеши" + kesha.getWeight());
+        System.out.println(kesha.getStatus());
+    }
+}
